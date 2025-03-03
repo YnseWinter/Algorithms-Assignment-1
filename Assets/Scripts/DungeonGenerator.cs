23fc128b451476ed0803c42d753817b94acab216
Ynse Winter <514704@student.saxion.nl>
2025-03-03 18:01:45 +0100

    It work now, just not as well as I want

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    List<RectInt> rooms = new List<RectInt>() { new RectInt(0, 0, 100, 50) };
+    List<RectInt> rooms = new List<RectInt>() { new RectInt(0, 0, 100, 100) };
     public int splitCount = 20;
     public Vector2 minRoomSize;
     public Vector2 maxRoomSize;
+
+    private RectInt currentRoom;
 
     private bool done;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -44,20 +46,23 @@ public class DungeonGenerator : MonoBehaviour
         //        break;
         //    }
         //}
-        RectInt room = FindBiggestRoom(true);
+        int biggestRoomIndex = FindBiggestRoom(true);
 
-        if (room.width > (minRoomSize.x * 2) + 2)
+        currentRoom = rooms[biggestRoomIndex];
+
+        if (currentRoom.width > (minRoomSize.x * 2) + 2)
         {
+            rooms.RemoveAt(biggestRoomIndex);
             float maxWidth = maxRoomSize.x;
-            if (room.width - minRoomSize.x < maxRoomSize.x)
+            if (currentRoom.width - minRoomSize.x < maxRoomSize.x)
             {
-                maxWidth = room.width - minRoomSize.x;
+                maxWidth = currentRoom.width - minRoomSize.x;
             }
-            RectInt roomA = room;
-            RectInt roomB = room;
+            RectInt roomA = currentRoom;
+            RectInt roomB = currentRoom;
             int randomValue = (int)Random.Range(minRoomSize.x + 2, maxWidth);
             roomA.width = randomValue;
-            roomB.xMin = randomValue + room.xMin - 1;
+            roomB.xMin = randomValue + currentRoom.xMin - 1;
 
 
             rooms.Add(roomA);
@@ -80,28 +85,50 @@ public class DungeonGenerator : MonoBehaviour
         //rooms.Add(roomA);
         //rooms.Add(roomB);
 
-        for (int i = 0; i < rooms.Count; i++)
+        //for (int i = 0; i < rooms.Count; i++)
+        //{
+        //    if (rooms[i].height > (minRoomSize.y * 2) + 2)
+        //    {
+        //        RectInt room = rooms[i];
+        //        float maxHeight = maxRoomSize.x;
+        //        if (rooms[i].height - minRoomSize.y < maxRoomSize.y)
+        //        {
+        //            maxHeight = rooms[i].height - minRoomSize.y;
+        //        }
+        //        rooms.RemoveAt(i);
+        //        RectInt roomA = room;
+        //        RectInt roomB = room;
+        //        int randomValue = (int)Random.Range(minRoomSize.y + 2, maxHeight);
+        //        roomA.height = randomValue;
+        //        roomB.yMin = randomValue + room.yMin - 1;
+
+
+        //        rooms.Add(roomA);
+        //        rooms.Add(roomB);
+        //        break;
+        //    }
+        //}
+        int biggestRoomIndex = FindBiggestRoom(false);
+
+        currentRoom = rooms[biggestRoomIndex];
+
+        if (currentRoom.height > (minRoomSize.y * 2) + 2)
         {
-            if (rooms[i].height > (minRoomSize.y * 2) + 2)
+            float maxHeight = maxRoomSize.x;
+            if (currentRoom.height - minRoomSize.y < maxRoomSize.y)
             {
-                RectInt room = rooms[i];
-                float maxHeight = maxRoomSize.x;
-                if (rooms[i].height - minRoomSize.y < maxRoomSize.y)
-                {
-                    maxHeight = rooms[i].height - minRoomSize.y;
-                }
-                rooms.RemoveAt(i);
-                RectInt roomA = room;
-                RectInt roomB = room;
-                int randomValue = (int)Random.Range(minRoomSize.y + 2, maxHeight);
-                roomA.height = randomValue;
-                roomB.yMin = randomValue + room.yMin - 1;
-
-
-                rooms.Add(roomA);
-                rooms.Add(roomB);
-                break;
+                maxHeight = currentRoom.height - minRoomSize.y;
             }
+            rooms.RemoveAt(biggestRoomIndex);
+            RectInt roomA = currentRoom;
+            RectInt roomB = currentRoom;
+            int randomValue = (int)Random.Range(minRoomSize.y + 2, maxHeight);
+            roomA.height = randomValue;
+            roomB.yMin = randomValue + currentRoom.yMin - 1;
+
+
+            rooms.Add(roomA);
+            rooms.Add(roomB);
         }
     }
 
@@ -111,32 +138,49 @@ public class DungeonGenerator : MonoBehaviour
         StartCoroutine(GenerateDungeon());
     }
 
-    void FindBiggestRoom(bool width)
+    int FindBiggestRoom(bool width)
     {
         RectInt biggestRoom = RectInt.zero;
         int biggestRoomIndex = 0;
         if (width)
         {
-            for(int i = 0; i > rooms.Count; i++)
+            for(int i = 0; i < rooms.Count; i++)
             {
                 if (rooms[i].width > biggestRoom.width)
                 {
                     biggestRoom = rooms[i];
                     biggestRoomIndex = i;
                 }
+                else if (rooms[i].width == biggestRoom.width)
+                {
+                    if (rooms[i].height > biggestRoom.height)
+                    {
+                        biggestRoom = rooms[i];
+                        biggestRoomIndex = i;
+                    }
+                }
             }
         }
         else
         {
-            for (int i = 0; i > rooms.Count; i++)
+            for (int i = 0; i < rooms.Count; i++)
             {
                 if (rooms[i].height > biggestRoom.height)
                 {
                     biggestRoom = rooms[i];
                     biggestRoomIndex = i;
                 }
+                else if (rooms[i].height == biggestRoom.height)
+                {
+                    if (rooms[i].width > biggestRoom.width)
+                    {
+                        biggestRoom = rooms[i];
+                        biggestRoomIndex = i;
+                    }
+                }
             }
         }
+        return biggestRoomIndex;
     }
 
     // Update is called once per frame
@@ -160,7 +204,8 @@ public class DungeonGenerator : MonoBehaviour
             {
                 SplitHorizontally();
             }
-            yield return new WaitForSeconds(.1f);
+            //yield return new WaitForSeconds(.1f);
+            yield return null;
         }
 
 
